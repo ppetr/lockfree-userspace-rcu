5843a0c0860b70bff94cabf27726bb86fc6d70fa
Petr Pudlak <petr.mvd+git@gmail.com>
2021-02-07 12:28:33 +0000

    Move `Refcount` next to the `Refcounted` class.

--- refcount_struct.h
+++ refcount_struct.h
@@ -44,50 +44,6 @@
 
 namespace refptr {
 
-class Refcount {
- public:
-  constexpr Refcount() : count_{1} {}
-
-  // Increments the reference count. Imposes no memory ordering.
-  inline void Inc() {
-    // No need for any synchronization/ordering, as the value is not inspected
-    // at all.
-    count_.fetch_add(1, std::memory_order_relaxed);
-  }
-
-  // Returns whether the atomic integer is 1.
-  inline bool IsOne() {
-    // This thread must observe the correct value, including any prior
-    // modifications by other threads.
-    return count_.load(std::memory_order_acquire) == 1;
-  }
-
-  // Returns `true` iff the counter's value is zero after the decrement
-  // operation. In such a case the caller must destroy the referenced object,
-  // and the counter's state becomes undefined.
-  //
-  // A caller should pass `expect_one = true` if there is a reasonable chance
-  // that there is only a single reference to the object. This allows slight
-  // performane optimization when reqesting the appropriate memory barriers.
-  inline bool Dec(bool expect_one = false) {
-    // This thread must observe the correct value if `refcount` reaches zero,
-    // including any prior modifications by other threads. All other threads
-    // must observe the result of the operation.
-    if (expect_one && IsOne()) {
-      // Knowing the object will be destructed, we don't decrement the counter.
-      // This way, we save the _release operation_ that would be needed for
-      // decrementing it below.
-      return true;
-    }
-    int32_t refcount = count_.fetch_sub(1, std::memory_order_acq_rel);
-    assert(refcount >= 0);
-    return refcount == 0;
-  }
-
- private:
-  std::atomic<int32_t> count_;
-};
-
 // Owns a block of memory large enough to store a properly aligned instance of
 // `T` and additional `size` number of elements of type `A`.
 template <typename T, typename A = char>
@@ -145,6 +101,50 @@ class Placement {
 
   size_t size_;
   char* allocation_;
+};
+
+class Refcount {
+ public:
+  constexpr Refcount() : count_{1} {}
+
+  // Increments the reference count. Imposes no memory ordering.
+  inline void Inc() {
+    // No need for any synchronization/ordering, as the value is not inspected
+    // at all.
+    count_.fetch_add(1, std::memory_order_relaxed);
+  }
+
+  // Returns whether the atomic integer is 1.
+  inline bool IsOne() {
+    // This thread must observe the correct value, including any prior
+    // modifications by other threads.
+    return count_.load(std::memory_order_acquire) == 1;
+  }
+
+  // Returns `true` iff the counter's value is zero after the decrement
+  // operation. In such a case the caller must destroy the referenced object,
+  // and the counter's state becomes undefined.
+  //
+  // A caller should pass `expect_one = true` if there is a reasonable chance
+  // that there is only a single reference to the object. This allows slight
+  // performane optimization when reqesting the appropriate memory barriers.
+  inline bool Dec(bool expect_one = false) {
+    // This thread must observe the correct value if `refcount` reaches zero,
+    // including any prior modifications by other threads. All other threads
+    // must observe the result of the operation.
+    if (expect_one && IsOne()) {
+      // Knowing the object will be destructed, we don't decrement the counter.
+      // This way, we save the _release operation_ that would be needed for
+      // decrementing it below.
+      return true;
+    }
+    int32_t refcount = count_.fetch_sub(1, std::memory_order_acq_rel);
+    assert(refcount >= 0);
+    return refcount == 0;
+  }
+
+ private:
+  std::atomic<int32_t> count_;
 };
 
 template <typename T>
