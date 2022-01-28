27be900fadb26043aea93da5e954d3efa0262ab0
Petr P. <petr.mvd+git@gmail.com>
2022-01-28 21:05:48 +0100

    Add a generic RCU implementation based on `Local3StateRcu`

---
+++ rcu.h
@@ -0,0 +1,127 @@
+#include <type_traits>
+#include <utility>
+
+#include "absl/container/flat_hash_set.h"
+#include "local_3state_rcu.h"
+
+namespace simple_rcu {
+
+// Generic, user-space RCU implementation with fast, atomic, lock-free reads.
+//
+// `T` must be copyable. Commonly it's `std::shared_ptr<const U>`.
+template <typename T>
+class Rcu {
+  static_assert(std::is_default_constructible<T>::value,
+                "T must be default constructible");
+  static_assert(std::is_copy_constructible<T>::value &&
+                    std::is_copy_assignable<T>::value,
+                "T must be copy constructible and assignable");
+
+ public:
+  class Local;
+
+  // Holds a read reference to a RCU value for the current thread.
+  // The reference is guaranteed to be stable during the lifetime of `ReadRef`.
+  // Callers are expected to limit the lifetime of `ReadRef` to as short as
+  // possible.
+  // Thread-compatible (but not thread-safe), reentrant.
+  class ReadRef final {
+   public:
+    ReadRef(ReadRef&& other) : ReadRef(other.registrar_) {}
+    ReadRef(const ReadRef& other) : ReadRef(other.registrar_) {}
+    ReadRef& operator=(ReadRef&&) = delete;
+    ReadRef& operator=(const ReadRef&) = delete;
+
+    ~ReadRef() { registrar_.read_depth_--; }
+
+    const T* operator->() const { return &**this; }
+    T* operator->() { return &**this; }
+    const T& operator*() const { return *registrar_.local_rcu_.Read(); }
+    T& operator*() { return registrar_.local_rcu_.Read(); }
+
+   private:
+    ReadRef(Local& registrar) : registrar_(registrar) {
+      if (registrar_.read_depth_++ == 0) {
+        registrar_.local_rcu_.TriggerRead();
+      }
+    }
+
+    Local& registrar_;
+
+    friend class Local;
+  };
+
+  // Interface to the RCU local to a particular reader thread.
+  // Construction and destruction are thread-safe operations, but the `Read()`
+  // method is only thread-compatible. Callers are expected to construct a
+  // separate `Local` instance for each reader thread.
+  class Local final {
+   public:
+    // Thread-safe.
+    Local(Rcu& rcu) : rcu_(rcu), read_depth_(0), local_rcu_() {
+      absl::MutexLock mutex(&rcu_.lock_);
+      rcu_.threads_.insert(this);
+      Update(rcu_.value_);
+    }
+    ~Local() {
+      absl::MutexLock mutex(&rcu_.lock_);
+      rcu_.threads_.erase(this);
+    }
+
+    // Obtains a read snapshot to the current value held by the RCU.
+    // This is a very fast, lock-free and atomic operation.
+    // Thread-compatible, but not thread-safe.
+    ReadRef Read() { return ReadRef(*this); }
+
+   private:
+    // Thread-compatible.
+    void Update(T value) {
+      std::swap(local_rcu_.Update(), value);
+      local_rcu_.TriggerUpdate();
+      // As a small performance optimization, destroy old `value` only after
+      // triggering update with the new value.
+    }
+
+    Rcu& rcu_;
+    // Incremented with each `ReadRef` instance. Ensures that `TriggerRead` is
+    // invoked only for the outermost `ReadRef`, keeping its value unchanged
+    // for its whole lifetime.
+    int_fast16_t read_depth_;
+    Local3StateRcu<T> local_rcu_;
+
+    friend class Rcu;
+  };
+
+  // Constructs a RCU with an initial value `T()`.
+  Rcu() : Rcu(T()) {}
+  Rcu(T initial_value)
+      : lock_(), value_(std::move(initial_value)), threads_() {}
+
+  // Updates `value` in all registered `Local` threads.
+  // Returns the previous value. Note that the previous value can still be
+  // observed by readers that haven't obtained a fresh `ReadRef()` instance.
+  //
+  // This method isn't tied in any particular way to a `Local` instance
+  // corresponding to the current thread, and can be called also by threads
+  // that have no `Local` instance at all.
+  //
+  // Thread-safe.
+  T Update(T value) {
+    absl::MutexLock mutex(&lock_);
+    std::swap(value_, value);
+    for (Local* thread : threads_) {
+      thread->Update(value_);
+    }
+    return value;
+  }
+
+ private:
+  absl::Mutex lock_;
+  // The current value that has been distributed to all thread-`Local`
+  // instances.
+  T value_ GUARDED_BY(lock_);
+  // List of registered thread-`Local` instances.
+  absl::flat_hash_set<Local*> threads_ GUARDED_BY(lock_);
+};
+
+}  // namespace simple_rcu
---
+++ rcu_test.cc
@@ -0,0 +1,55 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rcu.h"
+
+#include "gtest/gtest.h"
+
+namespace simple_rcu {
+namespace {
+
+TEST(RcuTest, UpdateAndRead) {
+  Rcu<int> rcu;
+  Rcu<int>::Local local1(rcu);
+  rcu.Update(42);
+  Rcu<int>::Local local2(rcu);
+  EXPECT_EQ(*local1.Read(), 42)
+      << "Thread registered prior Update must receive the value";
+  EXPECT_EQ(*local2.Read(), 42)
+      << "Thread registered after Update must also receive the value";
+}
+
+TEST(RcuTest, ThreadLocalUpdateAndRead) {
+  static Rcu<int> rcu;
+  static thread_local Rcu<int>::Local local(rcu);
+  rcu.Update(42);
+  EXPECT_EQ(*local.Read(), 42) << "Thread-local must receive the value";
+}
+
+TEST(RcuTest, ReadRemainsStable) {
+  Rcu<int> rcu(42);
+  Rcu<int>::Local local(rcu);
+  auto read_ref1 = local.Read();
+  rcu.Update(73);
+  EXPECT_EQ(*read_ref1, 42)
+      << "The first reference must hold its value past Update()";
+  auto read_ref2 = local.Read();
+  EXPECT_EQ(*read_ref1, 42)
+      << "The first reference must hold its value past another Read()";
+  EXPECT_EQ(*read_ref2, 42)
+      << "A nested reference must have the same value as an outer one";
+}
+
+}  // namespace
+}  // namespace simple_rcu
