d8981e4e4ffd1766a9fcbbc9849b96822cb5f487
Petr Pudlak <petr.mvd+git@gmail.com>
2022-02-01 10:22:25 +0100

    Add `ReverseRcu` class that implements reverse flow of information

---
+++ simple_rcu/reverse_rcu.h
@@ -0,0 +1,125 @@
+#include <type_traits>
+#include <utility>
+
+#include "absl/container/flat_hash_set.h"
+#include "absl/utility/utility.h"
+#include "simple_rcu/local_3state_rcu.h"
+
+namespace simple_rcu {
+
+// Class dual to `Rcu<T>`. The flow of information is reversed - from writers
+// ("readers") that actually store information and then it's collected from all
+// of them during the collect ("update") phase.
+//
+// `T` must define `T& operator+=(T&&)` (or a variant with any compatible
+// argument type) to combine values from thread-`Local` variables to a single
+// one.
+//
+// This is a low-level class, on top of which we can build a more user-friendly
+// interface for collecting metrics.
+template <typename T>
+class ReverseRcu {
+ public:
+  class Local;
+
+  // Holds a read reference to a RCU value for the current thread.
+  // The reference is guaranteed to be stable during the lifetime of `WriteRef`.
+  // Callers are expected to limit the lifetime of `WriteRef` to as short as
+  // possible.
+  // Thread-compatible (but not thread-safe), reentrant.
+  class WriteRef final {
+   public:
+    WriteRef(WriteRef&& other) : WriteRef(other.registrar_) {}
+    WriteRef(const WriteRef& other) : WriteRef(other.registrar_) {}
+    WriteRef& operator=(WriteRef&&) = delete;
+    WriteRef& operator=(const WriteRef&) = delete;
+
+    ~WriteRef() {
+      if (--registrar_.read_depth_ == 0) {
+        registrar_.local_rcu_.TriggerRead();
+      }
+    }
+
+    T* operator->() { return &**this; }
+    T& operator*() { return registrar_.local_rcu_.Read(); }
+
+   private:
+    WriteRef(Local& registrar) : registrar_(registrar) {
+      registrar_.read_depth_++;
+    }
+
+    Local& registrar_;
+
+    friend class Local;
+  };
+
+  // Interface to the RCU, local to a particular writer thread.
+  // Construction and destruction are thread-safe operations, but the `Write()`
+  // method is only thread-compatible. Callers are expected to construct a
+  // separate `Local` instance for each reader thread.
+  class Local final {
+   public:
+    // Thread-safe.
+    Local(ReverseRcu& rcu) LOCKS_EXCLUDED(rcu.lock_)
+        : rcu_(rcu), read_depth_(0), local_rcu_() {
+      absl::MutexLock mutex(&rcu_.lock_);
+      rcu_.threads_.insert(this);
+    }
+    ~Local() LOCKS_EXCLUDED(rcu_.lock_) {
+      absl::MutexLock mutex(&rcu_.lock_);
+      rcu_.value_ += Collect();
+      rcu_.threads_.erase(this);
+    }
+
+    // Obtains a read snapshot to the current value held by the RCU.
+    // This is a very fast, lock-free and atomic operation.
+    // Thread-compatible, but not thread-safe.
+    WriteRef Write() { return WriteRef(*this); }
+
+   private:
+    // Thread-compatible.
+    T Collect() EXCLUSIVE_LOCKS_REQUIRED(rcu_.lock_) {
+      local_rcu_.TriggerUpdate();
+      return absl::exchange(local_rcu_.Update(), T());
+    }
+
+    ReverseRcu& rcu_;
+    // Incremented with each `WriteRef` instance. Ensures that `TriggerRead` is
+    // invoked only for the outermost `WriteRef`, keeping its value unchanged
+    // for its whole lifetime.
+    int_fast16_t read_depth_;
+    Local3StateRcu<T> local_rcu_;
+
+    friend class ReverseRcu;
+  };
+
+  // Constructs a RCU with an initial value `T()`.
+  ReverseRcu() : lock_(), value_(), threads_() {}
+
+  // Reads values from all registered `Local` instances, including ones that
+  // have been destroyed since the last call.
+  // Returns the collected value and resets the internal variable to `T()`.
+  //
+  // This method isn't tied in any particular way to a `Local` instance
+  // corresponding to the current thread, and can be called also by threads
+  // that have no `Local` instance at all.
+  //
+  // Thread-safe.
+  T Collect() LOCKS_EXCLUDED(lock_) {
+    absl::MutexLock mutex(&lock_);
+    for (Local* thread : threads_) {
+      value_ += thread->Collect();
+    }
+    return absl::exchange(value_, T());
+  }
+
+ private:
+  absl::Mutex lock_;
+  // The current value that has been distributed to all thread-`Local`
+  // instances.
+  T value_ GUARDED_BY(lock_);
+  // List of registered thread-`Local` instances.
+  absl::flat_hash_set<Local*> threads_ GUARDED_BY(lock_);
+};
+
+}  // namespace simple_rcu
---
+++ simple_rcu/reverse_rcu_test.cc
@@ -0,0 +1,83 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "simple_rcu/reverse_rcu.h"
+
+#include <memory>
+
+#include "gtest/gtest.h"
+
+namespace simple_rcu {
+namespace {
+
+TEST(ReverseRcuTest, WriteAndCollect) {
+  ReverseRcu<int> rcu;
+  ReverseRcu<int>::Local local1(rcu);
+  {
+    ReverseRcu<int>::Local local2(rcu);
+    *local2.Write() += 10;
+  }
+  *local1.Write() += 1;
+  EXPECT_EQ(rcu.Collect(), 11)
+      << "Should receive value from both live and terminated threads";
+}
+
+TEST(ReverseRcuTest, WriteAndCollectMoveable) {
+  struct Value {
+    Value() : value(0) {}
+    Value(Value&&) = default;
+    Value(const Value&) = delete;
+    Value& operator=(Value&&) = default;
+    Value& operator=(const Value&) = delete;
+
+    Value& operator+=(Value&& other) {
+      value += other.value;
+      return *this;
+    }
+
+    int value;
+  };
+  ReverseRcu<Value> rcu;
+  ReverseRcu<Value>::Local local(rcu);
+  local.Write()->value += 42;
+  EXPECT_EQ(rcu.Collect().value, 42) << "Should receive a moved value";
+}
+
+TEST(ReverseRcuTest, ThreadLocalWriteAndCollect) {
+  static ReverseRcu<int> rcu;
+  static thread_local ReverseRcu<int>::Local local(rcu);
+  *local.Write() += 42;
+  EXPECT_EQ(rcu.Collect(), 42) << "Thread-local must receive the value";
+}
+
+TEST(ReverseRcuTest, WriteRemainsStable) {
+  ReverseRcu<int> rcu;
+  ReverseRcu<int>::Local local(rcu);
+  auto write_ref1 = local.Write();
+  *write_ref1 = 42;
+  EXPECT_EQ(rcu.Collect(), 0) << "The value should not be collected yet";
+  EXPECT_EQ(*write_ref1, 42)
+      << "The first reference must hold its value past Collect()";
+  {
+    auto write_ref2 = local.Write();
+    EXPECT_EQ(*write_ref2, 42)
+        << "A nested reference must have the same value as an outer one";
+  }
+  EXPECT_EQ(*write_ref1, 42) << "The first reference must hold its value past "
+                                "another Write() destructor";
+  EXPECT_EQ(rcu.Collect(), 0) << "The value should not be collected still";
+}
+
+}  // namespace
+}  // namespace simple_rcu
